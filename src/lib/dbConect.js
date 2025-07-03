c32e440a811134dbe6eabeb098edfbe559a609b7
Jesus Gonzalez <jgonzalez@wigilabs.com>
2025-07-03 17:19:56 -0500

    feat(deploy): mejoras config

--- src/lib/dbConect.js
+++ src/lib/dbConect.js
@@ -1,18 +1,26 @@
 import mongoose from "mongoose";
 
-const URI_MONGO = process.env.URL_DB
+const URI_MONGO = process.env.URL_DB;
+
+let isConnected = false;
 
 const ConectarBD = async () => {
-    try {
-        await mongoose.connect(URI_MONGO, {
-            bufferCommands: false,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
-}
+  if (isConnected) return;
 
-export default ConectarBD
+  try {
+    const db = await mongoose.connect(URI_MONGO, {
+      bufferCommands: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = db.connections[0].readyState === 1;
+
+    console.log("MongoDB conectado");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+export default ConectarBD;
